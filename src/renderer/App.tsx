ac2839436cb1c9e5e8c7e1793ed6ee3960867051
Jacob Daitzman <jdtzmn@gmail.com>
2025-04-18 04:07:21 -0400

    Add QR code interface

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,3 +1,13 @@
+import { useState } from 'react'
+import StreamingQRCode from './StreamingQRCode'
+import { generateSessionUuid } from './sessionUuid'
+
 export default function App() {
-  return <div className="text-center">Hello World</div>
+  const [sessionUuid, setSessionUuid] = useState(generateSessionUuid())
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <StreamingQRCode uuid={sessionUuid} />
+    </div>
+  )
 }
---
+++ src/renderer/StreamingQRCode.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+import { QRCodeSVG } from 'qrcode.react'
+
+const REFRESH_RATE = 25 // hz
+
+interface StreamingQRCodeProps {
+  uuid: string
+}
+
+export default function StreamingQRCode({ uuid }: StreamingQRCodeProps) {
+  // Update a timestamp state every REFRESH_RATE
+  const [timestamp, setTimestamp] = useState(Date.now())
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimestamp(Date.now())
+    }, 1000 / REFRESH_RATE)
+
+    return () => clearInterval(interval)
+  }, [])
+
+  return (
+    <div>
+      <QRCodeSVG value={`${uuid}@${timestamp}`} />
+      <p>{timestamp}</p>
+    </div>
+  )
+}

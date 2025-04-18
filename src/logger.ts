89b783449e6db4a2be59e7e172a83c1648b77f47
Jacob Daitzman <jdtzmn@gmail.com>
2025-04-18 02:05:57 -0400

    Set up logger structure

---
+++ src/logger.ts
@@ -0,0 +1,7 @@
+export function startLogger() {
+  console.log('Starting logger');
+}
+
+export function stopLogger() {
+  console.log('Stopping logger');
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'node:path';
 import started from 'electron-squirrel-startup';
+import {startLogger, stopLogger} from './logger';
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) {
@@ -28,7 +29,10 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', () => {
+  createWindow();
+  startLogger();
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -45,6 +49,10 @@ app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
+});
+
+app.on('before-quit', () => {
+  stopLogger();
 });
 
 // In this file you can include the rest of your app's specific main process

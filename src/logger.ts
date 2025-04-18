3b7a4bceb38cbf34f4ec1992c452b39efa14bb8c
Jacob Daitzman <jdtzmn@gmail.com>
2025-04-18 02:50:50 -0400

    Get keylogger working

--- src/logger.ts
+++ src/logger.ts
@@ -1,7 +1,20 @@
+import { uIOhook } from 'uiohook-napi'
+
 export function startLogger() {
   console.log('Starting logger');
+
+  uIOhook.on('keydown', (event) => {
+    console.log(event);
+  })
+
+  uIOhook.on('keyup', (event) => {
+    console.log(event);
+  })
+  
+  uIOhook.start();
 }
 
 export function stopLogger() {
   console.log('Stopping logger');
+  uIOhook.stop();
 }

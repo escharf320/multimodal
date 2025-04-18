0addf83bc4eec60ee2a7938a35a4d13c045dd5a6
Jacob Daitzman <jdtzmn@gmail.com>
2025-04-18 02:56:11 -0400

    Format the files

--- src/logger.ts
+++ src/logger.ts
@@ -1,20 +1,20 @@
 import { uIOhook } from 'uiohook-napi'
 
 export function startLogger() {
-  console.log('Starting logger');
+  console.log('Starting logger')
 
   uIOhook.on('keydown', (event) => {
-    console.log(event);
+    console.log(event)
   })
 
   uIOhook.on('keyup', (event) => {
-    console.log(event);
+    console.log(event)
   })
-  
-  uIOhook.start();
+
+  uIOhook.start()
 }
 
 export function stopLogger() {
-  console.log('Stopping logger');
-  uIOhook.stop();
+  console.log('Stopping logger')
+  uIOhook.stop()
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { app, BrowserWindow } from 'electron';
-import path from 'node:path';
-import started from 'electron-squirrel-startup';
-import {startLogger, stopLogger} from './logger';
+import { app, BrowserWindow } from 'electron'
+import path from 'node:path'
+import started from 'electron-squirrel-startup'
+import { startLogger, stopLogger } from './logger'
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) {
-  app.quit();
+  app.quit()
 }
 
 const createWindow = () => {
@@ -16,44 +16,46 @@ const createWindow = () => {
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
-  });
+  })
 
   // and load the index.html of the app.
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
-    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL)
   } else {
-    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+    mainWindow.loadFile(
+      path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`),
+    )
   }
-};
+}
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', () => {
-  createWindow();
-  startLogger();
-});
+  createWindow()
+  startLogger()
+})
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit();
+    app.quit()
   }
-});
+})
 
 app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
+    createWindow()
   }
-});
+})
 
 app.on('before-quit', () => {
-  stopLogger();
-});
+  stopLogger()
+})
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.

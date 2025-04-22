a0e9d65aefebd48d8f47cf0f621c5a6437cc8cba
Jacob Daitzman <jdtzmn@gmail.com>
2025-04-21 23:42:39 -0400

    Fix typo

--- typeformar/spacebar_detection/space_detector_lstm.py
+++ typeformar/spacebar_detection/space_detector_lstm.py
@@ -103,7 +103,7 @@ for epoch in range(EPOCHS):
 
 # See what the scores are after training
 with torch.no_grad():
-    first_test_sequence = test_sequences[0]
+    first_test_sequence = dataset[0]
     feature_sequence, ground_truth = first_test_sequence
     out_scores = model(feature_sequence)
     print("Prediction:")

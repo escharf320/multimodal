f3ec9d7da5fcc957e467b1d2cbfd147ea15084a0
Eli <escharf@mit.edu>
2025-04-29 16:36:13 -0400

    adding fucntion to segment joints by words

--- typeformar/dataset_generation/test_timestamp_inference.py
+++ typeformar/dataset_generation/test_timestamp_inference.py
@@ -27,19 +27,47 @@ for timestamp, joints in timestamp_joints:
 key_of_interest = 57
 
 # Test out key_pressed_v1 function and record the time it takes
-space_pressed_truths = key_pressed_v3(
-    logger_dicts, timestamp_joints, key_of_interest) 
+# space_pressed_truths = key_pressed_v3(
+#     logger_dicts, timestamp_joints, key_of_interest) 
 
-words = words_from_log(log_path)
+timestamped_words = words_from_log(log_path)
 
-joint_words = segment_joints_by_space_pressed(timestamp_joints, space_pressed_truths)
-
-#write space_pressed_truths to a file
-with open("space_pressed_truths.txt", "w") as f:
-    for item in space_pressed_truths:
-        f.write("%s\n" % item)
+# joint_words = segment_joints_by_space_pressed(timestamp_joints, space_pressed_truths)
 
 
-print(len(words), " words found in log file")
-print(len(joint_words)) 
-# print(joint_words[:1], " joint words found in video file")
+
+# print(words, " words found in log file")
+# print(len(joint_words)) 
+# print(joint_words)
+
+def segment_joints_by_word(timestamp_joints, timestamped_words, buffer=100):
+    """
+    Segments joints based on words.
+    
+    Args:
+        timestamp_joints: List of joint data with timestamps
+        timestamped_words: List of words with their corresponding timestamps
+    
+    Returns:
+        A list of tuples containing the joint data and the corresponding word.
+    """
+    segmented_joints = {}
+
+    for word, (start_time, end_time) in timestamped_words:
+        joints_list = []
+        for timestamp, jnts in timestamp_joints:
+            if start_time-buffer <= timestamp <= end_time+buffer:
+                joints_list.append(jnts)
+        
+
+        segmented_joints[(word, start_time)] = joints_list
+    
+    return segmented_joints
+
+segmented_joints = segment_joints_by_word(timestamp_joints, timestamped_words)
+
+print("Number of words: ", len(timestamped_words))
+print("Number of segmented joints: ", len(segmented_joints))
+for word, joints in segmented_joints.items():
+    print(f"Word: {word[0]}, Start Time: {word[1]}, Joints: {len(joints)}")
+# print("Segmented Joints: ", segmented_joints)

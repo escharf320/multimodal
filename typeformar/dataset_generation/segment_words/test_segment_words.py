5980dc41d74ab8425be4d6c49c5dac4f925ed7b3
Jacob Daitzman <jdtzmn@gmail.com>
2025-05-14 14:13:55 -0400

    Clean up test segment words

--- typeformar/dataset_generation/segment_words/test_segment_words.py
+++ typeformar/dataset_generation/segment_words/test_segment_words.py
@@ -1,8 +1,7 @@
 from find_words import words_from_log
+import os
 
 #### TESTING ####
-log_path= os.path.join(
-    os.path.dirname(__file__), "..", "..", "..", "data", "d1t2.log"
-)
-t = words_from_log(log_path)
-print(t)
+log_path = os.path.join(os.path.dirname(__file__), "..", "..", "..", "data", "d2t1.log")
+log = words_from_log(log_path)
+print(log)

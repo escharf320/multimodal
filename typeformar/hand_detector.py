cc66fc7e8fa560c6aab6970fba9bef82ec2954df
Jacob Daitzman <jdtzmn@gmail.com>
2025-05-14 14:13:40 -0400

    Clean up hand detector

--- typeformar/hand_detector.py
+++ typeformar/hand_detector.py
@@ -7,9 +7,9 @@ hands = mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5
 mp_drawing = mp.solutions.drawing_utils
 
 # Start capturing from the webcam
-# cap = cv2.VideoCapture(1)
-video_path = "/Users/eli/Downloads/typing_example2.mov"
-cap = cv2.VideoCapture(video_path)
+cap = cv2.VideoCapture(1)
+# video_path = "/Users/eli/Downloads/typing_example2.mov"
+# cap = cv2.VideoCapture(video_path)
 
 # print(cv2.getBuildInformation())
 
@@ -40,7 +40,7 @@ while True:
     # Display the frame with hand landmarks
     cv2.imshow("Hand Detection", frame)
 
-    #Print the hand landmarks for debugging
+    # Print the hand landmarks for debugging
     # print("Hand landmarks:")
     # if results.multi_hand_landmarks:
     #     for hand_landmarks in results.multi_hand_landmarks:
@@ -48,7 +48,7 @@ while True:
     #             print(f"Landmark {id}: ({lm.x}, {lm.y}, {lm.z})")
 
     # Break the loop if 'q' is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 # Release the video capture and close the windows
